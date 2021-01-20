0b9376e038d3d4c2de8ba64ab3de3445263307e4
bramiller1989 <bramiller1989@gmail.com>
2021-01-19 20:49:49 -0600

    show reference type

--- Program.cs
+++ Program.cs
@@ -24,6 +24,10 @@ namespace ReferenceAndValueTypes
             var number = 1;
             Increment(number);
             Console.WriteLine(number);
+
+            var person = new Person() {Age = 20};
+            MakeOld(person);
+            Console.WriteLine(person.Age);
         }
 
         public static void Increment(int number)
